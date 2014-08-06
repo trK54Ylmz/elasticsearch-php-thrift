dc03fa28ced287450aa34515a40750a0af303a58
Tarık Yılmaz <tarikyilmaz@yandex.com.tr>
2014-08-06 20:37:24 +0300

    New system

--- src/ElasticSearch.php
+++ src/ElasticSearch.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__. '/Thrift/ClassLoader/ThriftClassLoader.php';
+require_once __DIR__ . '/Thrift/ClassLoader/ThriftClassLoader.php';
 
 use Thrift\ClassLoader\ThriftClassLoader;
 
@@ -8,9 +8,8 @@ $loader->registerNamespace('Thrift', __DIR__);
 $loader->registerDefinition('Elasticsearch', __DIR__);
 $loader->register();
 
-use Thrift\Protocol\TBinaryProtocol;
+use \Thrift\Protocol\TBinaryProtocolAccelerated;
 use Thrift\Transport\TSocket;
-use Thrift\Transport\THttpClient;
 use Thrift\Transport\TBufferedTransport;
 use Thrift\Exception\TException;
 
@@ -20,46 +19,51 @@ use Elasticsearch\Method;
 
 use Elasticsearch\RestClient as ESRest;
 
-class ESThriftClient
+class ElasticSearch
 {
     protected $client;
     protected $socket;
     protected $transport;
     protected $connected;
 
-    public function __construct($host = 'localhost', $port = 9500){
-        try{
+    public function __construct($host = 'localhost', $port = 9500) {
+        try {
             $this->socket = new TSocket($host, $port);
             $this->transport = new TBufferedTransport($this->socket, 1024, 1024);
-            $protocol = new TBinaryProtocol($this->transport);
+            $protocol = new TBinaryProtocolAccelerated($this->transport);
             $this->client = new ESRest($protocol);
             $this->transport->open();
             $this->connected = true;
-        }catch(TException $e){
+        } catch(TException $e) {
             $this->connected = false;
         }
     }
 
-    public function isConnected(){
+    /**
+     * Check Thrift transport opened
+     *
+     * @return bool
+     */
+    public function isConnected() {
         return $this->connected;
     }
 
-    public function sendRequest($body = null, $uri = null, $method = Method::POST){
-        try{
+    public function sendRequest($body = null, $uri = null, $method = Method::POST) {
+        try {
             $r = new RestRequest();
             $r->method = $method;
             $r->uri = $uri;
             $r->body = $body;
             return $this->client->execute($r);
-        }catch(TException $e){
+        } catch(TException $e) {
             $response = new RestResponse();
             $response->status = 404;
             return $response;
         }
     }
 
-    public function Close(){
-        if($this->connected){
+    public function Close() {
+        if($this->connected) {
             $this->client = null;
             $this->transport->close();
             $this->socket->close();
@@ -67,7 +71,7 @@ class ESThriftClient
         }
     }
 
-    public function __destruct(){
+    public function __destruct() {
         $this->Close();
     }
 }
